09880eb70fd4daed7eeeeee81c7f2b9dd975a09b
Michael Lucy <mlucy@stripe.com>
2017-11-08 17:44:57 -0800

    mlucy.py

---
+++ architectures/pytorch/mlucy.py
@@ -0,0 +1,105 @@
+import gym
+import torch
+import torch.nn as nn
+import time
+import random
+import math
+from torch.autograd import Variable
+
+def from_gym(x):
+    return torch.from_numpy(x).transpose(0, 2).unsqueeze(0).float()
+
+class BreakoutActorCritic(nn.Module):
+    def __init__(self, input_shape, output_size):
+        super().__init__()
+        in_channels = input_shape[1]
+        self.visual_parser = nn.Sequential(
+            nn.Conv2d(in_channels, 16, kernel_size=8, stride=4), nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=4, stride=2), nn.ReLU())
+        input_sz = self.visual_parser(Variable(torch.zeros(input_shape))).view(-1).data.shape[0]
+        self.common = nn.Sequential(
+            nn.Linear(input_sz, 256), nn.ReLU(),
+            nn.Linear(256, 256), nn.ReLU())
+        self.action = nn.Sequential(nn.Linear(256, output_size), nn.Softmax())
+        self.value = nn.Linear(256, 1)
+
+    def forward(self, state0, state1):
+        state = Variable(torch.cat((state0, state1), 1)).cuda()
+        c = self.common(self.visual_parser(state).view(-1).unsqueeze(0))
+        return self.action(c), self.value(c)
+
+class BreakoutPlayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.breakout = gym.make('Breakout-v4')
+        self.steps = -1
+        self.setup()
+        state_shape = list(from_gym(self.breakout.observation_space.low).shape)
+        state_shape[1] *= 2 # we have one frame of history
+        self.model = BreakoutActorCritic(state_shape, self.breakout.action_space.n)
+        self.opt = torch.optim.Adam(self.model.parameters(), lr=5e-7)
+        self.value_criterion = nn.MSELoss()
+        self.gamma = 0.99
+        self.last_action = None
+
+    def epsilon(self):
+        return 0.05 + 0.7*math.exp(-1*self.steps/2000)
+
+    def setup(self):
+        self.steps = self.steps + 1
+        self.states = [from_gym(self.breakout.reset())]
+        self.states.append(from_gym(self.breakout.step(0)[0]))
+        self.last_action = None
+
+    def step(self):
+        action_probs, value = self.model.forward(*self.states)
+        action = action_probs.multinomial()
+
+        if self.last_action is not None:
+            self.opt.zero_grad()
+            ideal_value = self.last_reward + self.gamma*Variable(value.data)
+            if not self.last_action_fake:
+                value_diff = ideal_value - self.last_value
+                self.last_action.reinforce(value_diff.data)
+                self.last_action.backward(retain_graph=True)
+            value_loss = self.value_criterion(self.last_value, ideal_value)
+            value_loss.backward()
+            self.opt.step()
+
+        self.last_action = action
+        self.last_value = value
+
+        self.last_action_fake = random.random() < self.epsilon()
+        to_take = action.data[0][0]
+        if self.last_action_fake:
+            to_take = random.randrange(4)
+
+        if self.steps % 3 == 0:
+            print('%d (fake: %d) (epsilon: %.02f)' %
+                  (self.steps, self.last_action_fake, self.epsilon()))
+            print('Action: %s' % ['.', 'F', 'R', 'L'][to_take])
+            print('Value: %s' % value.data[0][0])
+            d = action_probs.data[0]
+            print('.=%.02f F=%.02f R=%.02f L=%.02f' % (d[0], d[1], d[2], d[3]))
+            self.breakout.render()
+
+        state, self.last_reward, done, _ = self.breakout.step(to_take)
+        if done:
+            self.opt.zero_grad()
+            ideal_value = Variable(torch.Tensor(((0,),)).cuda())
+            value_loss = self.value_criterion(self.last_value, ideal_value)
+            value_loss.backward(retain_graph=True)
+            value_diff = ideal_value - self.last_value
+            self.last_action.reinforce(value_diff.data)
+            self.last_action.backward()
+            self.opt.step()
+
+            self.setup()
+        else:
+            self.states = [self.states[1], from_gym(state)]
+
+bp = BreakoutPlayer().cuda()
+bp.breakout.render()
+while True:
+    bp.step()
+
